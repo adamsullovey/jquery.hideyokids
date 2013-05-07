ba8debb6c439fba5c56e96b1f78b1a5f531c1306
Adam Sullovey <adam.sullovey@gmail.com>
2013-05-06 22:08:33 -0400

    first commit after sooooooo much work

---
+++ src/jquery.hideyokids.js
@@ -0,0 +1,27 @@
+(function( $ ){
+
+
+	var methods = {
+
+	/**
+	* all up in your global namespace
+	*/
+	$.fn.hideyokids = function( type ) {
+		this.children().hide();
+	};
+
+	$.fn.showyokids = function( type ) {
+		this.children().show();
+	};
+
+
+	$.fn.hideyowife = function( type ) {
+		this.next().hide();
+	};
+
+	$.fn.showyowife = function( type ) {
+		this.next().show();
+	};
+
+
+})( jQuery );
