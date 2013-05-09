c0727bf1ca2b213d0aace9977871e27d297df3e8
Adam Sullovey <adam.sullovey@gmail.com>
2013-05-09 01:59:22 -0400

    added chainability to plugin and demonstration

--- src/jquery.hideyokids.js
+++ src/jquery.hideyokids.js
@@ -5,19 +5,23 @@
 	*/
 	$.fn.hideyokids = function( type ) {
 		this.children().hide();
+		return this;
 	};
 
 	$.fn.showyokids = function( type ) {
 		this.children().show();
+		return this;
 	};
 
 
 	$.fn.hideyowife = function( type ) {
 		this.next().hide();
+		return this;
 	};
 
 	$.fn.showyowife = function( type ) {
 		this.next().show();
+		return this;
 	};
 
 
