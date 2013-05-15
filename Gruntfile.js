d49a6650ff719cbb656c77a0f3678f3d3f37fbfb
Adam Sullovey <adam.sullovey@gmail.com>
2013-05-15 00:39:40 -0400

    setup Grunt contrib for updating jquery manifest file pased on info in package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,23 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'src/jquery.<%= pkg.name %>.js',
+        dest: 'build/jquery.<%= pkg.name %>.min.js'
       }
-    }
+    },
+    jquerymanifest: {
+           options: {
+               source: grunt.file.readJSON('package.json')
+           }
+        }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jquerymanifest');
+
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'jquerymanifest']);
 
 };
